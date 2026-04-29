e42cfa3b2e22796fa314b721bf2187ce3f87ac95
Aayush Mani <aayushmani1449f@gmail.com>
2026-04-29 14:31:59 +0530

    Add HashMap UC2

--- Main.java
+++ Main.java
@@ -14,5 +14,20 @@ public class Main {
             myHashMap.add(word, value);
         }
         System.out.println(myHashMap);
+
+        System.out.println("\nTesting HashMap UC2:");
+        String paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+        String[] paraWords = paragraph.toLowerCase().split(" ");
+        for (String word : paraWords) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null) {
+                value = 1;
+            } else {
+                value = value + 1;
+            }
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println(myLinkedHashMap);
     }
 }
